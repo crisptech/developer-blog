cfbb5baaf4b7b175238c2945cff212cf0d48df9f
crisptech <crisptech@hotmail.com>
2022-06-20 15:51:14 +0100

    feat: blog posts dynamically load in timeline component

--- components/blog-timeline.tsx
+++ components/blog-timeline.tsx
@@ -9,89 +9,17 @@ import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
 import { Typography } from "@mui/material";
 import { format } from "date-fns";
 import { Post } from "../types/posts";
-import { map } from "ramda";
 import Link from "next/link";
 
 type BlogTimeLineProps = {
-  posts: Pick<Record<string, Post>, string>[];
+  posts: Post[];
 };
-
-map((post) => {
-  const date = post.date;
-  const formattedDate = format(new Date(date), "do MMM yyyy");
-
-  return (
-            <TimelineItem>
-              <TimelineOppositeContent color="text.secondary">
-                {formattedDate}
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineDot />
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent sx={{ paddingTop: "0.1em" }}>
-                <Link href={`/posts/${post.id}`}>
-                  <Typography variant="h6">{post.title}</Typography>
-                  <Typography>{post.description}</Typography>
-                </Link>
-              </TimelineContent>
-            </TimelineItem>
-          );
-        })}
-        {/* <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            {formattedDate}
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent sx={{ paddingTop: "0.1em" }}>
-            <Typography variant="h6">Title 1</Typography>
-            <Typography>Lorem ipsu lala land</Typography>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            10:00 am
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Code</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            12:00 am
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Sleep</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            9:00 am
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Repeat</TimelineContent>
-        </TimelineItem> */}
-      </Timeline>
-    </>
-, posts)
-  )
-});
 
 const BlogTimeline: React.FC<BlogTimeLineProps> = ({ posts }) => {
   return (
     <>
       <Timeline position="alternate">
-        {posts.map((post) => {
+        {posts.map((post, index) => {
           const date = post.date;
           const formattedDate = format(new Date(date), "do MMM yyyy");
 
@@ -100,62 +28,27 @@ const BlogTimeline: React.FC<BlogTimeLineProps> = ({ posts }) => {
               <TimelineOppositeContent color="text.secondary">
                 {formattedDate}
               </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineDot />
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent sx={{ paddingTop: "0.1em" }}>
+              {index < posts.length - 1 ? (
+                <TimelineSeparator>
+                  <TimelineDot />
+                  <TimelineConnector />
+                </TimelineSeparator>
+              ) : (
+                <TimelineSeparator>
+                  <TimelineDot />
+                </TimelineSeparator>
+              )}
+              <TimelineContent
+                sx={{ paddingTop: "0.1em", paddingBottom: "5rem" }}
+              >
                 <Link href={`/posts/${post.id}`}>
                   <Typography variant="h6">{post.title}</Typography>
-                  <Typography>{post.description}</Typography>
                 </Link>
+                <Typography>{post.description}</Typography>
               </TimelineContent>
             </TimelineItem>
           );
         })}
-        {/* <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            {formattedDate}
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent sx={{ paddingTop: "0.1em" }}>
-            <Typography variant="h6">Title 1</Typography>
-            <Typography>Lorem ipsu lala land</Typography>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            10:00 am
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Code</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            12:00 am
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Sleep</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            9:00 am
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Repeat</TimelineContent>
-        </TimelineItem> */}
       </Timeline>
     </>
   );
--- selectors/selectVisiblePosts.ts
+++ selectors/selectVisiblePosts.ts
@@ -3,10 +3,14 @@ import { RootState } from "../app/store";
 import { selectGlobalPosts } from "./selectGlobalPosts";
 import { selectVisiblePostIds } from "./selectVisiblePostIds";
 import { map, pick } from "ramda";
+import { Post } from "../types/posts";
 
 export const selectVisiblePosts = createSelector(
   selectVisiblePostIds,
   selectGlobalPosts,
   (visiblePostIds, globalPosts) =>
-    map((id: string) => pick([id], globalPosts), visiblePostIds)
+    map((id: string) => {
+      const post = pick([id], globalPosts);
+      return post[id];
+    }, visiblePostIds)
 );
